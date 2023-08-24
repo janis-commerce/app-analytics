b5e7a3658eeafcbeeaaa7bf90eb5687903f4eec1
Gonzalo Lopez <gonzalo.lopez@fizzmod.com>
2023-08-24 17:09:36 -0300

    workflows fix

--- setupTest/jest.setup.js
+++ setupTest/jest.setup.js
@@ -7,9 +7,3 @@ jest.mock('@react-native-firebase/analytics', () =>
     logEvent: jest.fn(),
   })),
 );
-
-jest.mock('@janiscommerce/apps-helpers', () => ({
-  _esModule: true,
-  isString: jest.fn().mockReturnValue(true),
-  isObject: jest.fn().mockReturnValue(true)
-}))
